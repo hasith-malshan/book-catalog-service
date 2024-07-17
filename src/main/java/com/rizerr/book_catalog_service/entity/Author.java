f988de62c844b4ebc4356a1a42e1ceca0ee5735d
Hasith Malshan <hasher22542@gmail.com>
2024-07-16 23:11:10 -0700

    Column names added and Mappings with author.

--- src/main/java/com/rizerr/book_catalog_service/entity/Author.java
+++ src/main/java/com/rizerr/book_catalog_service/entity/Author.java
@@ -1,24 +1,29 @@
 package com.rizerr.book_catalog_service.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "author")
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+    @Column(name = "name")
     private String name;
+    @ManyToMany
+    private Set<Book> books;
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
