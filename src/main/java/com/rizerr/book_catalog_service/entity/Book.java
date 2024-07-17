e43f82dcca09061c7e21d023bf34f5e35d4ae222
Hasith Malshan <hasher22542@gmail.com>
2024-07-16 23:11:19 -0700

    Column names added and Mappings with Books.

--- src/main/java/com/rizerr/book_catalog_service/entity/Book.java
+++ src/main/java/com/rizerr/book_catalog_service/entity/Book.java
@@ -1,26 +1,31 @@
 package com.rizerr.book_catalog_service.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "book")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "isbn")
     private Integer isbn;
+    @Column(name = "title")
     private String title;
+    @Column(name = "price")
     private Double price;
-    private Author Author;
+    @ManyToMany(mappedBy = "books")
+    private Set<Author> author;
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
