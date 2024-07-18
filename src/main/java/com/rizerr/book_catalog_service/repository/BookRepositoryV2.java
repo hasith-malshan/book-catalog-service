263db43d2112e4fae97018db376ac61c700434f0
Hasith Malshan <hasher22542@gmail.com>
2024-07-18 12:33:05 +0530

    Same Query methods from the BookRepository implemented.

--- src/main/java/com/rizerr/book_catalog_service/repository/BookRepositoryV2.java
+++ src/main/java/com/rizerr/book_catalog_service/repository/BookRepositoryV2.java
@@ -1,7 +1,13 @@
 package com.rizerr.book_catalog_service.repository;
 
+import com.rizerr.book_catalog_service.entity.Book;
 import com.rizerr.book_catalog_service.entity.BookV2;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepositoryV2 extends JpaRepository<BookV2, Integer> {
+    Optional<Book> findByIsbn(Long isbn);
+    Optional<Book> findByTitle(String name);
+    Optional<Book> findByTitleStartingWith(String title);
 }
