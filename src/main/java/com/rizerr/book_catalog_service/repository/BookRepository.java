626a1d40a36509f1279c47ee5756d5282a836de5
Hasith Malshan <hasher22542@gmail.com>
2024-07-18 09:11:48 +0530

    Query Method implemented to getBooks by title.

--- src/main/java/com/rizerr/book_catalog_service/repository/BookRepository.java
+++ src/main/java/com/rizerr/book_catalog_service/repository/BookRepository.java
@@ -2,11 +2,15 @@ package com.rizerr.book_catalog_service.repository;
 
 import com.rizerr.book_catalog_service.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
     Optional<Book> findByIsbn(Long isbn);
     Optional<Book> findByTitle(String name);
+
+    Optional<Book> findByTitleStartingWith(String title);
 }
