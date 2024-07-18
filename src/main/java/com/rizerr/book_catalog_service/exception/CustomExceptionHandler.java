87ab5188973e0aa1bae3080e56323f050e9beef7
Hasith Malshan <hasher22542@gmail.com>
2024-07-18 12:25:58 +0530

    Method Implemented to HandleUnhandled Exception.

--- src/main/java/com/rizerr/book_catalog_service/exception/CustomExceptionHandler.java
+++ src/main/java/com/rizerr/book_catalog_service/exception/CustomExceptionHandler.java
@@ -12,6 +12,14 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<?> handleRegularException(Exception ex , WebRequest request)throws Exception{
+        ErrorMessage errorMessage = new ErrorMessage();
+        errorMessage.setMessage(ex.getMessage());
+        errorMessage.setDateTime(LocalDateTime.now());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(IllegalStateException.class)
     public final ResponseEntity<?> handleIllegalStateException(Exception ex, WebRequest webRequest) throws Exception{
         ErrorMessage errorMessage = new ErrorMessage();
