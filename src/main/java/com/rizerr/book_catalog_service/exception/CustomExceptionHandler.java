7008e780f5247f8f73d56283fb5ba8cdd8c87a49
Hasith Malshan <hasher22542@gmail.com>
2024-07-18 10:02:56 +0530

    CustomExceptionHandler created. and implemented Method to handle IllegalStateException.

---
+++ src/main/java/com/rizerr/book_catalog_service/exception/CustomExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.rizerr.book_catalog_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(IllegalStateException.class)
+    public final ResponseEntity<?> handleIllegalStateException(Exception ex, WebRequest webRequest) throws Exception{
+        ErrorMessage errorMessage = new ErrorMessage();
+        errorMessage.setMessage(ex.getMessage());
+        errorMessage.setDateTime(LocalDateTime.now());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+}
